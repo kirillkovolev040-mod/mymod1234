74785d935c37aa896248e1f8d1a13b9a81eeffcb
kirillkovolev040-mod <kirillkovolev040@gmail.com>
2026-05-26 16:59:00 +0300

    Update RightHandConfig.java

--- src/main/java/com/example/mymod/RightHandConfig.java
+++ src/main/java/com/example/mymod/RightHandConfig.java
@@ -5,19 +5,23 @@ public class RightHandConfig {
     public static float rightX = 0.12f; 
     public static float rightY = 0.10f;
     public static float rightZ = -0.45f;
-    public static int rightScalePercent = 30; 
+    public static int rightScalePercent = 30; // Изначально уменьшена на 30%
 
     // Настройки ЛЕВОЙ руки (Клавиша I)
     public static float leftX = -0.315f; 
     public static float leftY = 0.10f;
     public static float leftZ = -0.45f;
-    public static int leftScalePercent = 40; 
+    public static int leftScalePercent = 40; // Изначально уменьшена на 40%
     
-    // Анимации и визуал
+    // Анимации удара (0 - Дефолт, 1 - Плавный 1.7, 2 - Слайд, 3 - Хлыст)
     public static int swingMode = 1; 
+    
+    // Выбранный ID эффекта критов из сетки
     public static int activeParticleId = 0;
+    
+    // ID цвета прицела
     public static int crosshairColorId = 0; 
 
-    // Высота огня на экране в процентах (от 0% до 100%)
+    // Высота огня на экране в процентах (Регулируется в меню J на +-5%)
     public static int fireHeightPercent = 40; 
 }
