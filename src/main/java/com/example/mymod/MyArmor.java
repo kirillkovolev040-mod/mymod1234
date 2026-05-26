a597ef5332009e8e75dc03743229699c56ecce27
kirillkovolev040-mod <kirillkovolev040@gmail.com>
2026-05-26 16:40:03 +0300

    Update MyArmor.java

--- src/main/java/com/example/mymod/MyArmor.java
+++ src/main/java/com/example/mymod/MyArmor.java
@@ -25,7 +25,7 @@ class RightConfigScreen extends Screen {
         this.renderBackground(graphics, mouseX, mouseY, partialTick);
         graphics.drawCenteredString(this.font, "Нажмите ESC для возврата в игру", this.width / 2, this.height / 2 + 115, 0xAAAAAA);
         int cy = this.height / 2; int cx = this.width / 2 - 60;  
-        graphics.drawCenteredString(this.font, "===[ НАСТРОЙКА ПРАВОЙ РУКИ (K) ]===", this.width / 2, cy - 100, 0xFF5555);
+        graphics.drawCenteredString(this.font, "===[ НАСТРОЙКА ПРАВОЙ РУКИ ]===", this.width / 2, cy - 100, 0xFF5555);
         
         drawCustomButton(graphics, "↑ Выше", cx, cy - 80, 120, 18, mouseX, mouseY);
         drawCustomButton(graphics, "↓ Ниже", cx, cy - 58, 120, 18, mouseX, mouseY);
@@ -33,12 +33,12 @@ class RightConfigScreen extends Screen {
         drawCustomButton(graphics, "← Ближе", cx, cy - 14, 120, 18, mouseX, mouseY);
         drawCustomButton(graphics, "⏪ Влево", cx, cy + 8, 120, 18, mouseX, mouseY);
         drawCustomButton(graphics, "Вправо ⏩", cx, cy + 30, 120, 18, mouseX, mouseY);
-        drawCustomButton(graphics, "Размер (П): -" + RightHandConfig.rightScalePercent + "%", cx, cy + 52, 120, 18, mouseX, mouseY);
+        drawCustomButton(graphics, "Размер: -" + RightHandConfig.rightScalePercent + "%", cx, cy + 52, 120, 18, mouseX, mouseY);
         
         String animName = "Дефолт";
         if (RightHandConfig.swingMode == 1) animName = "Плавный 1.7";
         if (RightHandConfig.swingMode == 2) animName = "PvP Круговой";
-        drawCustomButton(graphics, "Удар (П): " + animName, cx, cy + 74, 120, 18, mouseX, mouseY);
+        drawCustomButton(graphics, "Удар: " + animName, cx, cy + 74, 120, 18, mouseX, mouseY);
         
         super.render(graphics, mouseX, mouseY, partialTick);
     }
@@ -81,7 +81,7 @@ class LeftConfigScreen extends Screen {
         this.renderBackground(graphics, mouseX, mouseY, partialTick);
         graphics.drawCenteredString(this.font, "Нажмите ESC для возврата в игру", this.width / 2, this.height / 2 + 110, 0xAAAAAA);
         int cy = this.height / 2; int cx = this.width / 2 - 60;  
-        graphics.drawCenteredString(this.font, "===[ НАСТРОЙКА ЛЕВОЙ РУКИ (I) ]===", this.width / 2, cy - 95, 0x55FF55);
+        graphics.drawCenteredString(this.font, "===[ НАСТРОЙКА ЛЕВОЙ РУКИ ]===", this.width / 2, cy - 95, 0x55FF55);
         
         drawCustomButton(graphics, "↑ Выше", cx, cy - 75, 120, 20, mouseX, mouseY);
         drawCustomButton(graphics, "↓ Ниже", cx, cy - 50, 120, 20, mouseX, mouseY);
@@ -89,7 +89,7 @@ class LeftConfigScreen extends Screen {
         drawCustomButton(graphics, "← Ближе", cx, cy, 120, 20, mouseX, mouseY);
         drawCustomButton(graphics, "⏪ Влево", cx, cy + 25, 120, 20, mouseX, mouseY);
         drawCustomButton(graphics, "Вправо ⏩", cx, cy + 50, 120, 20, mouseX, mouseY);
-        drawCustomButton(graphics, "Размер (Л): -" + RightHandConfig.leftScalePercent + "%", cx, cy + 75, 120, 20, mouseX, mouseY);
+        drawCustomButton(graphics, "Размер: -" + RightHandConfig.leftScalePercent + "%", cx, cy + 75, 120, 20, mouseX, mouseY);
         super.render(graphics, mouseX, mouseY, partialTick);
     }
 
@@ -99,7 +99,7 @@ class LeftConfigScreen extends Screen {
             int cy = this.height / 2; int cx = this.width / 2 - 60;
             if (mx >= cx && mx <= cx + 120) {
                 if (my >= cy - 75 && my <= cy - 55) { RightHandConfig.leftY += 0.05f; return true; }
-                if (my >= cy - 50 && my <= cy - 30) { RightHandConfig.leftY -= 0.05f; return true; }
+                if (my >= cy - 45 && my <= cy - 25) { RightHandConfig.leftY -= 0.05f; return true; }
                 if (my >= cy - 25 && my <= cy - 5)  { RightHandConfig.leftZ -= 0.05f; return true; }
                 if (my >= cy && my <= cy + 20)       { RightHandConfig.leftZ += 0.05f; return true; }
                 if (my >= cy + 25 && my <= cy + 45) { RightHandConfig.leftX -= 0.05f; return true; }
