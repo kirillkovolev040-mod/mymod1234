2b92d776e9fba21ff6c5b2a224bf2cda0ff9c119
kirillkovolev040-mod <kirillkovolev040@gmail.com>
2026-05-26 21:11:29 +0300

    Update MyKeyBindings.java

--- src/main/java/com/example/mymod/MyKeyBindings.java
+++ src/main/java/com/example/mymod/MyKeyBindings.java
@@ -7,21 +7,22 @@ import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import org.lwjgl.glfw.GLFW;
 
 public class MyKeyBindings {
-    public static final KeyMapping OPEN_RIGHT_CONFIG = new KeyMapping(
+    // Полностью русские названия переменных!
+    public static final KeyMapping ОТКРЫТЬ_ПРАВУЮ_КОНФИГУРАЦИЮ = new KeyMapping(
         "key.mymod.right_config",
         InputConstants.Type.KEYSYM,
         GLFW.GLFW_KEY_K, 
         "key.categories.mymod"
     );
 
-    public static final KeyMapping OPEN_LEFT_CONFIG = new KeyMapping(
+    public static final KeyMapping ОТКРЫТЬ_ЛЕВУЮ_КОНФИГУРАЦИЮ = new KeyMapping(
         "key.mymod.left_config",
         InputConstants.Type.KEYSYM,
         GLFW.GLFW_KEY_I, 
         "key.categories.mymod"
     );
 
-    public static final KeyMapping OPEN_PARTICLE_CONFIG = new KeyMapping(
+    public static final KeyMapping ОТКРЫТЬ_КОНФИГУРАЦИЮ_ЧАСТИЦ = new KeyMapping(
         "key.mymod.particle_config",
         InputConstants.Type.KEYSYM,
         GLFW.GLFW_KEY_J, 
